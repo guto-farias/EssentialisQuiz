b8a626f4961bb65fa587458d8a6f48ab4dd492f8
guto-farias <gutofariasdev@gmail.com>
2024-08-27 09:31:33 -0300

    add essentialis

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+//8083
+
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx aaaaaaaaaaaaa tototttttttttttttttttttttoto edit this screeaaaan.</Text>
+    </View>
+  );
+}
